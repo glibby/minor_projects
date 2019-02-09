5ae09f0f57d8bdd14e6f2c11c0de300ae8fa99a9
Max Cooper <maxmancoop@gmail.com>
2019-02-09 19:00:23 +0000

    Added hello world to c directory to make it feel right.

---
+++ c/hello/hello.c
@@ -0,0 +1,14 @@
+/**
+ * FILE: hello.c
+ * AUTHOR: Max Cooper
+ * PURPOSE: Just to say hello. The repo felt wrong without it.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", "Hello world");
+	exit(0);
+}
